20624547168544c470676f8cb40f74e07d60763b
Ketil Nordstad <ketilkn@gmail.com>
2017-10-30 17:38:48 +0100

    Shorten Khorne Daemons

--- bbl-scraper/export/filter.py
+++ bbl-scraper/export/filter.py
@@ -6,7 +6,7 @@ def race_short(input):
         return ""
     if "undead" in lowered_input or "renegades" in lowered_input:
         return input.split(" ")[-1]
-    if "underworld" in lowered_input or "necromantic" in lowered_input or "chosen" in lowered_input:
+    if "horne" in lowered_input or "underworld" in lowered_input or "necromantic" in lowered_input or "chosen" in lowered_input:
         return input.split(" ")[0]
     if "union" in lowered_input:
         return "{}lf".format(input[0])
