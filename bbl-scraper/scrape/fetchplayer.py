b286045fcec11c8f6e81340ae38e14a9425039a3
Ketil Nordstad <ketilkn@gmail.com>
2017-11-01 16:21:43 +0100

    Use correct base URL

--- bbl-scraper/scrape/fetchplayer.py
+++ bbl-scraper/scrape/fetchplayer.py
@@ -4,16 +4,16 @@ import requests
 from time import sleep
 import scrape.session
 
-base_url = "http://www.anarchy.bloodbowlleague.com/default.asp?p=pladm&pid={}"
+base_url = "http://www.anarchy.bloodbowlleague.com/default.asp?p=pl&pid={}"
 admin_url = "http://www.anarchy.bloodbowlleague.com/default.asp?p=pladm&pid={}"
 	
 def download_all_players():
     s = scrape.session.login(sys.argv[1], sys.argv[2])
-    for playerid in range(5, 3415):
+    for playerid in range(12, 3529):
         if scrape.session.download_to(s, admin_url.format(playerid), "input/admin-player-{}.html".format(playerid)):
             sleep(2)
             scrape.session.download_to(s, base_url.format(playerid), "input/player-{}.html".format(playerid))
-        sleep(3)
+        sleep(5)
 	
 def main():
     download_all_players()
