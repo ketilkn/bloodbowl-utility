b68a76a3f8e998a75f4b9ee4e1f2bf3be8ed0f72
Ketil Nordstad <ketilkn@gmail.com>
2017-11-04 21:14:02 +0100

    Create json cache for coach

--- bbl-scraper/coach/coach.py
+++ bbl-scraper/coach/coach.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+import os
+import time
+import json
 from coach import parse
 
 def dict_coaches(use_key="nick"):
@@ -12,8 +15,24 @@ def dict_coaches(use_key="nick"):
 def dict_coaches_by_uid():
     return dict_coaches("uid")
 
+def load_from_json():
+    data = open("input/json/coaches.json", "rb").read()
+    return json.loads(data.decode())
+
+
+def save_to_json(coaches):
+    data = json.dumps(coaches)
+    json_file = open("input/json/coaches.json", "wb")
+    json_file.write(data.encode())
+    json_file.close()
+    
+
 def list_coaches():
-    return parse.load()
+    if not os.path.isfile("input/json/coaches.json") or os.stat("input/json/coaches.json").st_mtime < os.stat("input/coaches-8.html").st_mtime:
+        coaches = parse.load()
+        save_to_json(coaches)
+        return coaches
+    return load_from_json()
 
 def find_uid_for_nick(coaches, nick):
     for the_coach in coaches.values():
