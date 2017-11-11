e176282eff0874f5f375b2dfc32df3a47efc9554
Ketil Nordstad <ketilkn@gmail.com>
2017-11-11 14:12:21 +0100

    Read player IDs from command line argument

--- bbl-scraper/scrape/fetchplayer.py
+++ bbl-scraper/scrape/fetchplayer.py
@@ -7,19 +7,17 @@ import scrape.session
 base_url = "http://www.anarchy.bloodbowlleague.com/default.asp?p=pl&pid={}"
 admin_url = "http://www.anarchy.bloodbowlleague.com/default.asp?p=pladm&pid={}"
 	
-def download_all_players():
+def download_all_players(fetch_list):
     s = scrape.session.login(sys.argv[1], sys.argv[2])
-    for playerid in range(12, 3529):
+    for playerid in fetch_list:
         if scrape.session.download_to(s, admin_url.format(playerid), "input/admin-player-{}.html".format(playerid)):
             sleep(2)
             scrape.session.download_to(s, base_url.format(playerid), "input/player-{}.html".format(playerid))
         sleep(5)
 	
 def main():
-    download_all_players()
-    #s = scrape.session.login(sys.argv[1], sys.argv[2])
-    #print(s.cookies)
-    #scrape.session.download_to(s, base_url.format(2807), "input/admin-player-2807.html")
+    fetch_list = sys.argv[3:] if len(sys.argv) > 3 else range(5, 3535)
+    download_all_players(fetch_list)
 
 
 if __name__ == "__main__":
