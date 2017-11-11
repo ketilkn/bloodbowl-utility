d55a41a9c5a482ab89ee6f8784b64d5766874484
Ketil Nordstad <ketilkn@gmail.com>
2017-11-11 14:11:34 +0100

    Use logging

--- bbl-scraper/player/load.py
+++ bbl-scraper/player/load.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """ Python libraries for Dr.Tide"""
 import logging
-LOG = logging.getLogger(__name__)
 from bs4 import BeautifulSoup
 from unicodedata import normalize
 import datetime
@@ -37,32 +36,35 @@ def setup_log(level):
 
     # add formatter to ch
     ch.setFormatter(formatter)
-    LOG.addHandler(ch)
+    logging.addHandler(ch)
 
 def main():
     print(__package__)
     print(__name__)
-    LOG.setLevel(logging.INFO)
-    setup_log(logging.DEBUG)
-    arguments = sys.argv[1:]
-    if "--debug" in arguments:
-        arguments.remove("--debug")
-        LOG.setLevel(logging.DEBUG)
-        LOG.debug("Log level debug")
-        print("debug???")
-    else:
-        print("itte debug")
-    interesting = arguments if len(sys.argv) > 0 else None
+
+    if "--debug" in sys.argv[1:]:
+        logging.basicConfig(level=logging.DEBUG)
+        logging.debug("Log level debug")
+
+    do_print = True if "--no-print" not in sys.argv[1:] else False
+
+    logging.basicConfig(level=logging.INFO)
+
+    arguments = list(filter(lambda x: not x.startswith("--"), sys.argv[1:]))
+    interesting = arguments if len(arguments) > 0 else None
+    logging.info("program started")
+    logging.debug("debug")
+    #setup_log(logging.DEBUG)
 
     path = "input/html/player/"
-    LOG.info("Loading players from %s", path)
+    logging.info("Loading players from %s", path)
     import pprint
     if interesting:
-        LOG.info("Looking for %s", interesting)
+        logging.info("Looking for %s", interesting)
     pp = pprint.PrettyPrinter(indent=4)
 
     for player in parse_path(path):
-        if not interesting or player["playerid"] in interesting:
+        if do_print and (not interesting or player["playerid"] in interesting):
             pp.pprint(player)
 
 if __name__ == "__main__":
