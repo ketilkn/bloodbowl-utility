58024758b70dfce3ba55eb8b2a7aca7c594fe9e2
Ketil Nordstad <ketilkn@gmail.com>
2017-12-06 23:52:39 +0100

    Order output by spp

--- bbl-scraper/player/display.py
+++ bbl-scraper/player/display.py
@@ -7,13 +7,14 @@ def plprint(p):
     print("{:>4} {:>32} {:>18} ca:{:>2} cl:{:>2} in:{:>2} mv:{:>2} td:{:>2} to:{:>3}".format(
         p["playerid"], p["playername"], p["position"], p["spp"]["casualty"], p["spp"]["completion"], p["spp"]["interception"], p["spp"]["mvp"], p["spp"]["td"], p["spp"]["total"]))
 
-def psa(ps):
+def psa(players):
     for p in players:
         plprint(p)
 
 def main():
     players = list(load_all())
-    psa(players)
+
+    psa(sorted(players, key=lambda p: int(p["spp"]["total"]) if p["spp"]["total"] else 0))
 
 
 if __name__=="__main__":
