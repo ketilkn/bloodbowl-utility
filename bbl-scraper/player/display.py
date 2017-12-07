9bb3d81067ff1f4c5d1fc79eaf098cbc7fa25795
Ketil Nordstad <ketilkn@gmail.com>
2017-12-07 19:56:29 +0100

    Add team, skills, injuries

--- bbl-scraper/player/display.py
+++ bbl-scraper/player/display.py
@@ -4,8 +4,19 @@ import player.player
 from .load import load_all
 
 def plprint(p):
-    print("{:>4} {:>32} {:>18} ca:{:>2} cl:{:>2} in:{:>2} mv:{:>2} td:{:>2} to:{:>3}".format(
-        p["playerid"], p["playername"], p["position"], p["spp"]["casualty"], p["spp"]["completion"], p["spp"]["interception"], p["spp"]["mvp"], p["spp"]["td"], p["spp"]["total"]))
+    print("{:>4} {:>32} {:>18} t:{:<4} c:{:>2} p:{:>3} i:{:>2} m:{:>2} g:{:>2} *:{:>3} {} {}".format(
+        p["playerid"], 
+        p["playername"],
+        p["position"], 
+        p["team"] if p["team"] else "0000", 
+        p["spp"]["casualty"], 
+        p["spp"]["completion"], 
+        p["spp"]["interception"], 
+        p["spp"]["mvp"], 
+        p["spp"]["td"], 
+        p["spp"]["total"],
+        ",".join(p["upgrade"]["normal"]+p["upgrade"]["extra"]+p["status"]["injury"]),
+        p["status"]["active"]["reason"]))
 
 def psa(players):
     for p in players:
