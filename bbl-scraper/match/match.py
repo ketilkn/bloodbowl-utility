15de08478e2b272b4fb5f18d76ba9076b847a7df
Ketil Nordstad <ketilkn@scan4news.com>
2017-12-20 09:46:59 +0100

    Add logger, reformat code

--- bbl-scraper/match/match.py
+++ bbl-scraper/match/match.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
-import sys
+import logging
 from match import load
 from match import parse
 
+LOG = logging.getLogger(__package__)
+
+
 def open_match(filename):
-    matchid = filename[filename.find("match-")+6:filename.rfind(".html")]
-        
-    return parse.parse_match( matchid, load.from_file(filename))
+    matchid = filename[filename.find("match-") + 6:filename.rfind(".html")]
+
+    return parse.parse_match(matchid, load.from_file(filename))
 
 
 def collate_gamedata(games, id=None):
@@ -19,17 +22,25 @@ def dict_games():
         games[g["matchid"]] = g
     return games
 
+
 def match_list():
     games = load.from_json()
-    return games 
+    return games
 
 
 def main():
     import sys
     import pprint
+
+    log_format = "[%(levelname)s:%(filename)s:%(lineno)s - %(funcName)20s ] %(message)s"
+    logging.basicConfig(level=logging.DEBUG, format=log_format)
+    LOG.info("Loading matches")
+
     matchid = sys.argv[1:] if len(sys.argv) > 1 else None
     matches = list(collate_gamedata(match_list(), matchid))
     pprint.pprint(matches, indent=4, width=160)
     if len(sys.argv[1:]) > 1: print("Found {} of {}".format(len(matches), len(sys.argv[1:])))
+
+
 if __name__ == "__main__":
     main()
