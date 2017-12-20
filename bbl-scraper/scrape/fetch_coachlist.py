e028a1ec417b126f46de50e84a7e937090ad5932
Ketil Nordstad <ketilkn@gmail.com>
2017-12-20 15:16:20 +0100

    Specify arguments for login

--- bbl-scraper/scrape/fetch_coachlist.py
+++ bbl-scraper/scrape/fetch_coachlist.py
@@ -7,7 +7,7 @@ import scrape.session
 coaches_url = "http://www.anarchy.bloodbowlleague.com/default.asp?p=co"
 	
 def download_coach_list():
-    s = scrape.session.login(sys.argv[1], sys.argv[2])
+    s = scrape.session.login("http://www.anarchy.bloodbowlleague.com/login.asp", username=sys.argv[1], password=sys.argv[2])
     scrape.session.download_to(s, coaches_url, "input/html/coach/coaches-8.html")
 	
 def main():
