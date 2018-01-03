956c5f7d4bb0c6ea8bf78636fb912ae8ed0f8136
Ketil Nordstad <ketilkn@scan4news.com>
2018-01-03 20:23:36 +0100

    Put order by spp in discrete function

--- bbl-scraper/stats/player_list.py
+++ bbl-scraper/stats/player_list.py
@@ -7,16 +7,22 @@ from importer.bbleague.defaults import BASEPATH
 LOG = logging.getLogger(__package__)
 
 
+def order_by_spp(players):
+    """order list of players by player.spp.total"""
+    return sorted(players,
+                  key=lambda p: int(p["spp"]["total"]) if p["spp"]["total"] else 0,
+                  reverse=True)
+
+
 def all_players(data, include_journeymen=False):
     """Convert players in collated data to list"""
     LOG.debug("Player count is %s", len(data["player"]) if "player" in data else "No 'player' in data")
-    if not "player" in data:
+    if "player" not in data:
         LOG.warning("No 'player' in data")
     if "player" in data and len(data["player"]) == 0:
         LOG.warning("Zero players in data")
 
-    players = sorted(data["player"].values(), key=lambda p: int(p["spp"]["total"]) if p["spp"]["total"] else 0, reverse=True)
-
+    players = order_by_spp(data["player"].values())
 
     if not include_journeymen:
         LOG.debug("Filter journeymen")
