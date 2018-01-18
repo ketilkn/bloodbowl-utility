a6ef78ca2b7e50c8ed6c958074a7bd069d3eb2d3
Ketil Nordstad <ketilkn@gmail.com>
2018-01-18 13:32:50 +0100

    Set download path to default

--- bbl-scraper/scrape/fetch_coachlist.py
+++ bbl-scraper/scrape/fetch_coachlist.py
@@ -1,14 +1,16 @@
 #!/bin/env python3
 import sys
-import requests
+import os.path
 from time import sleep
+import requests
 import scrape.session
+from importer.bbleague.defaults import BASEPATH
 
 coaches_url = "http://www.anarchy.bloodbowlleague.com/default.asp?p=co"
 	
 def download_coach_list():
     s = scrape.session.login("http://www.anarchy.bloodbowlleague.com/login.asp", username=sys.argv[1], password=sys.argv[2])
-    scrape.session.download_to(s, coaches_url, "input/html/coach/coaches-8.html")
+    scrape.session.download_to(s, coaches_url, os.path.join(BASEPATH, "html/coach/coaches-8.html"))
 	
 def main():
     download_coach_list()
--- bbl-scraper/scrape/fetch_teamlist.py
+++ bbl-scraper/scrape/fetch_teamlist.py
@@ -1,14 +1,16 @@
 #!/bin/env python3
 import sys
-import requests
+import os.path
 from time import sleep
+import requests
 import scrape.session
+from importer.bbleague.defaults import BASEPATH
 
 teams_url = "http://www.anarchy.bloodbowlleague.com/default.asp?p=te"
 	
 def download_team_list():
     s = scrape.session.login("http://www.anarchy.bloodbowlleague.com/login.asp", username=sys.argv[1], password=sys.argv[2])
-    scrape.session.download_to(s, teams_url, "input/html/team/teams-8.html")
+    scrape.session.download_to(s, teams_url, os.path.join(BASEPATH, "html/team/teams-8.html"))
 	
 def main():
     download_team_list()
