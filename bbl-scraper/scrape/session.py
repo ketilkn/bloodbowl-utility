7492bc7705f8eb5e68f8268d4ad13687c211ce55
Ketil Nordstad <ketilkn@scan4news.com>
2018-04-09 14:48:05 +0200

    Fix bug in OK check. Add logging

--- bbl-scraper/scrape/session.py
+++ bbl-scraper/scrape/session.py
@@ -1,47 +1,54 @@
 #!/usr/bin/env python3
 import requests
 import sys
+import logging
 
-
-
+LOG = logging.getLogger(__package__)
 
 
 def download_to(session, url, target):
+    LOG.debug("Downloading %s to %s", url, target)
     response = session.get(url)
     if not response.history and response.status_code==200: 
         html = response.text
         try:
             open(target, "w").write(html)
-            print(" Wrote {} to {}".format(url, target))
+            LOG.info(" Wrote {} to {}".format(url, target))
             return True
         except OSError:
-            print(" Failed writing {} to {}".format(url, target))
+            LOG.error(" Failed writing {} to {}".format(url, target))
     else:
-        print(" Server error {} to {}".format(url, response.status_code))
+        LOG.error(" Server error {} to {}".format(url, response.status_code))
     return False
 
 
 def verify_session(session, response = None):
     #TODO check if session is logged in
+    LOG.debug("verifying session on %s", response.url)
     return response.status_code == 200 and response.url.endswith("/default.asp?p=adm")
 
 
 def login(url, username, password):
+    LOG.debug("Logging in to %s using %s", url, username)
     s = requests.session()
     r = s.post(url, data={"user":username, "pass":password})
 
     if verify_session(s, r):
         return s
-    #FIXME Throw exception ? / return None?
-    print("Could not verify session")
+    LOG.error("Could not verify session")
     sys.exit("Could not verify session")
+
 
 def new_session():
     return requests.session()
 
+
 def main():
+    log_format = "[%(levelname)s:%(filename)s:%(lineno)s - %(funcName)20s ] %(message)s"
+    logging.basicConfig(level=logging.DEBUG, format=log_format)
     session=login("http://www.anarchy.bloodbowlleague.com/login.asp", sys.argv[1], sys.argv[2])
     print("{}".format(session.cookies))
+
 
 if __name__ == "__main__":
     main()
