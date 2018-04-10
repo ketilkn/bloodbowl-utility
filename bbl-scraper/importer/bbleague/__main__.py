923e49de885ba8fbcab3f45166effa5bc86fd758
Ketil Nordstad <ketilkn@scan4news.com>
2018-04-10 22:15:38 +0200

    Use argument parser

--- bbl-scraper/importer/bbleague/__main__.py
+++ bbl-scraper/importer/bbleague/__main__.py
@@ -2,6 +2,7 @@
 """  default functionality for bbleague importer """
 import sys
 import logging
+import argparse
 
 import importer.bbleague.update
 
@@ -10,10 +11,15 @@ LOG = logging.getLogger(__package__)
 
 def main():
     log_format = "[%(levelname)s:%(filename)s:%(lineno)s - %(funcName)20s ] %(message)s"
-    logging.basicConfig(level=logging.DEBUG, format=log_format)
+    logging.basicConfig(level=logging.DEBUG if "--debug" in sys.argv else logging.INFO, format=log_format)
+
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("site", help="[Site] from bbl-scrape.ini")
+    argparser.add_argument("debug", action="store_true")
+    args = argparser.parse_args()
 
     section = "test" if len(sys.argv) < 2 else sys.argv[1]
-    config = importer.bbleague.update.load_config(section)
+    config = importer.bbleague.update.load_config(args.site)
     importer.bbleague.update.import_bloodbowlleague(config)
 
 
